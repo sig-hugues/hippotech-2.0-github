2a034c6d591def663b340ff266309c69269e5037
sig-hugues <100399627+sig-hugues@users.noreply.github.com>
2023-11-17 15:17:21 +0100

    Update BlogAnalyticsRepository.java

--- java-api/src/main/java/com/hippotech/api/data/BlogAnalyticsRepository.java
+++ java-api/src/main/java/com/hippotech/api/data/BlogAnalyticsRepository.java
@@ -33,7 +33,7 @@ public class BlogAnalyticsRepository {
             log.error("Created subscriber table.");
 
             sql = "SELECT * FROM subscribers where email = '" + email + "';";
-            int rowsSelect = statement.executeSelect(sql);
+            statement.execute(sql);
             
             sql = "INSERT INTO subscribers (ID, email) values (" + nextId.toString() + ", '" + email + "')";
             nextId++;
